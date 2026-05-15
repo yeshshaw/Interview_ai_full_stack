fed0342986ad5cfefca212e35364328d56f2e6a5
yesh shaw <nihassid@gmail.com>
2026-05-15 15:05:57 +0530

    fixed upstash redis tls

--- Backend/src/config/redis.js
+++ Backend/src/config/redis.js
@@ -1,31 +1,23 @@
 const redis = require("redis");
 
 const redisClient = redis.createClient({
-
-    url: process.env.REDIS_URL
-
+    url: process.env.REDIS_URL,
+    socket: {
+        tls: true,
+        rejectUnauthorized: false
+    }
 });
 
 async function connectRedis() {
-
     try {
-
         await redisClient.connect();
-
         console.log("Redis connected");
-
     } catch (err) {
-
         console.log(err);
-
     }
-
 }
 
 module.exports = {
-
     redisClient,
-
     connectRedis
-
 };
