cdec16350e24cc426490d5e7ea31c9cd2d843191
yesh shaw <nihassid@gmail.com>
2026-05-15 13:25:56 +0530

    fixed import case

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom"
 import Login from './features/auth/pages/Login'
 import Register from './features/auth/pages/Register'
-import { Protected } from "./features/auth/components/protected"
+import { Protected } from "./features/auth/components/Protected"
 import  Home from "./features/interview/pages/Home"
 import  Interview from "./features/interview/pages/Interview"
 import LandingPage from "./features/landingpage/LandingPage"
