4871bb3f70f47d06f97fe796b5319753769e6f0f
yesh shaw <nihassid@gmail.com>
2026-05-15 13:23:32 +0530

    updated cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,8 +10,8 @@ const app = express() ;
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin :"http://localhost:5173" ,
-  credentials : true
+  origin: "https://interview-ai-full-stack-1.onrender.com",
+    credentials : true
 }))
 app.set('trust proxy', 1)
 
